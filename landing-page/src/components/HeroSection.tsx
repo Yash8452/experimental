3e95b0b94d7f2e8769153a52be0ff392e6cb79a8
YASH GHARAT <53621256+Yash8452@users.noreply.github.com>
2024-03-12 22:47:28 +0000

    hero section responsivness fixed

--- landing-page/src/components/HeroSection.tsx
+++ landing-page/src/components/HeroSection.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
 
 
 
 function HeroSection() {
+    const word = [
+        {
+            text: "Build",
+        },
+        {
+            text: "awesome",
+        },
+        {
+            text: "apps",
+        },
+        {
+            text: "with",
+        },
+        {
+            text: "Aceternity.",
+        },
+    ]
     return (<>
         <div className="h-full w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
             {/* Radial gradient for the container to give a faded look */}
@@ -16,21 +35,20 @@ function HeroSection() {
                     className="-top-40 left-0 md:left-60 md:-top-20"
                     fill="white"
                 />
-                <div className="p-4 relative z-10 w-full text-center" >
-                    <h1
-                        className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
-                    >Master the art of music</h1>
-                    <p
-                        className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg italic mx-auto"
-                    >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+                <div className="p-4 flex flex-col items-center justify-center text-center space-y-0  w-full" >
+
+
+
+                    <TextGenerateEffect className=" md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+                        words={"Hello,"} />
+
+                    <TextGenerateEffect className=" md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+                        words={"I'm Yash GHarat"} />
+
+                    <TextGenerateEffect className="md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400" words={"A passionate  Developer"} />;
                     <div className="mt-4">
                         <Link href={"/courses"}>
-                            {/* <Button
-                        borderRadius="1.75rem"
-                        className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                    >
-                        Explore courses
-                    </Button> */}
+
                         </Link>
                     </div>
                 </div>
